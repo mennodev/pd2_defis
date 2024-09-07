3a9ab1249974c1ef195016ca590a47de59ea7f1a
mennodev <mennodev@hotmail.com>
2024-09-07 15:08:40 +0200

    - add updated streamlit app to new remote

---
+++ 1_🏡_Homepage.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# setup page config
+st.set_page_config(page_title="Home", page_icon="🏡")
+
+# title
+st.title('Copernicus High-Cadence Monitoring for the EU Green Deal')
+# logo gallery
+st.image('data/images/homepage/logo.png', caption='Companies in the consortium executing DEFIS/2022/OP/0012')
+
+st.header('Welcome to the page dedicated to the Pilote Demonstrator 2 : Regional Biodiversity Monitoring')
+st.image('data/images/homepage/algaes.jpg', caption='Green algaes in the bay of Saint-Brieuc.')
